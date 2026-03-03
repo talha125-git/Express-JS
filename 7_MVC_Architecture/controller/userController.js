01a9ecc029b856892a29b491f79e1103ddf3aa63
talha125-git <malikabutalharaheem@gamil.com>
2026-03-03 03:39:06 -0800

    add 7_MVC_Architecture

---
+++ 7_MVC_Architecture/controller/userController.js
@@ -0,0 +1,9 @@
+import { usersList } from "../model/userModel.js";
+
+export function handleUsers (req, resp){
+    const userData = usersList()
+    console.log(userData);
+    
+    resp.render('user',{users:userData});
+    
+}
---
+++ 7_MVC_Architecture/model/userModel.js
@@ -0,0 +1,5 @@
+//  Stores or contain data in userModel
+
+export function usersList (){
+    return ['malik','abutalha','raheem','khan','ahmad']
+}
