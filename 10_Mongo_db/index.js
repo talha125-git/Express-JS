61e7b7bcbd1601ace08b7ee066990fb02563f9ef
talha125-git <malikabutalharaheem@gamil.com>
2026-03-03 11:46:15 -0800

    add Mongo_db

---
+++ 10_Mongo_db/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+
+const app = express();
+
+const dbName = "school";
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+async function dbConnection() {
+    
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection("students");
+
+        const result = await collection.find().toArray();
+
+        console.log(result);
+   
+}
+
+dbConnection();
+
+app.listen(3200)
