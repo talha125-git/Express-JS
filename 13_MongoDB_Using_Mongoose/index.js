285715d452235f1d83cec06d32901f46884df8e0
talha125-git <malikabutalharaheem@gamil.com>
2026-03-09 01:51:23 -0700

    add to no_13 Make Post method REST API with Mongoose to insert data

--- 13_MongoDB_Using_Mongoose/index.js
+++ 13_MongoDB_Using_Mongoose/index.js
@@ -3,6 +3,8 @@ import express from "express";
 import studentModel from "./model/studentModel.js";
 
 const app = express();
+app.use(express.json());
+
 await mangoose.connect("mongodb://localhost:27017/school").then(()=>{
     console.log("_________cconneted_________");
     
@@ -12,6 +14,28 @@ app.get("/", async (req,resp)=>{
     const studentData = await studentModel.find()
 
     resp.send(studentData)
+})
+
+// Make Post method REST API with Mongoose to insert data
+app.post("/save",async (req,resp)=>{
+    console.log(req.body);
+    // Validation
+    const {id,name,age,dept}= req.body
+    if(!req.body || !id || !name ||!age ||!dept){
+        resp.send({
+            message: "data not store",
+            success:false,
+            storedinfo:null
+        })
+    }
+
+    const studentData = await studentModel.create(req.body)
+    
+    resp.send({
+        message: "data saved",
+        success:true,
+        storedinfo:studentData
+    })
 })
 
 app.listen(3200)
